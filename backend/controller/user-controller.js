ca8b1605ecd1a07c6389f5ae3debb255bbee7d99
Man-Kaur <man.kaur86998@gmail.com>
2025-01-20 15:27:04 +0530

    Authentication

--- backend/controller/user-controller.js
+++ backend/controller/user-controller.js
@@ -16,7 +16,10 @@ exports.signup = async (req, res) => {
             return res.status(400).json({ message: "User already exists" });
         }
 
-        const user = new User({ username, email, password });
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = new User({ username, email, password: hashedPassword });
         await user.save();
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
